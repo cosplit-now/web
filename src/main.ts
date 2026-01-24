fcd7845f85e0954f4b35b561508370fcd42c3594
Xinqi Mu <hi@muxinqi.com>
2026-01-24 12:11:27 -0500

    add Sentry Vue integration to project dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import router from "./router";
+import * as Sentry from "@sentry/vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.mount('#app')
+Sentry.init({
+  app,
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+  integrations: [Sentry.browserTracingIntegration({ router })],
+  // Tracing
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/cosplit-api\.xinqi\.mu/],
+  // Logs
+  enableLogs: true,
+});
+
+app.use(router);
+app.mount("#app");
